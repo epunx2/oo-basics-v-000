74248bec0467985e9587a258144be6b518db7efd
Eric Chrobak <eric@ericchrobak.com>
2018-05-15 01:40:18 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
 # Make your shoe class here!
+class Shoe
+
+  attr_accessor :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def properties
+    brand, color, size, material, condition
+
+  end
+
+end
